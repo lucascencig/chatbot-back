86f95539a2ed48bdc79db72babc65f6978a469b2
lucascencig <cenciglucas@gmail.com>
2023-10-10 05:38:49 -0300

    express and cors

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const http = require('http');
-const server = http.createServer();
+const express = require('express');
+const cors = require('cors'); // Agrega la importación de cors
+
+const app = express();
+const server = http.createServer(app);
 
 const io = require('socket.io')(server, {
-  cors: { origin: '*' }
+  cors: {
+    origin: '*' // Esto permitirá cualquier origen, pero en producción, debes configurar esto adecuadamente
+  }
 });
+
+app.use(cors()); // Habilita el middleware de cors
 
 io.on('connection', (socket) => {
   console.log('Conectado al servidor');
 
   socket.on('chat_message', (data) => {
     io.emit('chat_message', data);
-  })
+  });
 });
 
 server.listen(8084);
