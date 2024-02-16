486c04c0efcded4821bb71c2f16c37b44e935065
lucascencig <cenciglucas@gmail.com>
2024-02-16 18:20:16 -0300

    fixing code

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 const http = require('http');
 const express = require('express');
-const cors = require('cors'); // Agrega la importación de cors
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: '*' // Esto permitirá cualquier origen, pero en producción, debes configurar esto adecuadamente
+    origin: '*'
   }
 });
 
-app.use(cors()); // Habilita el middleware de cors
+app.use(cors());
 
 
 
@@ -24,13 +24,6 @@ io.on('connection', (socket) => {
 
   socket.on('chat_message', (data) => {
     console.log(`Mensaje recibido de ${data.username}: ${data.message}`);
-
-    // Asegúrate de que data contenga el nombre de usuario
-    // const messageData = {
-    //   senderName: data.senderName,
-    //   message: data.message
-    // };
-
     io.emit('chat_message', data); // Reenvía el mensaje a todos los clientes
   });
 });
